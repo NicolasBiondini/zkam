40d2a97ffe6aec366a68ee6ab28ecda2e42f6b13
Yago Pajariño <yagopajarino@outlook.com>
2024-08-04 12:42:33 -0300

    Agrega logo a navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,6 +4,7 @@ import WalletButton from "./WalletButton";
 function NavBar() {
   return (
     <nav className="flex flex-row w-full justify-between items-center px-4 h-[80px]">
+      <img src="zkam-icon-0-no-background.png" alt="zkam-logo" className="w-12" />
       <p className="font-bold text-lg">ZKam</p>
       <WalletButton />
     </nav>
